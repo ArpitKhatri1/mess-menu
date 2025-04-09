5c754198bd15703450df125a3d67acce78f218f3
ArpitKhatri1 <arpitkumarkhatri@gmail.com>
2025-04-09 19:17:42 +0530

    added vercel analytics

--- mess-site/src/app/layout.tsx
+++ mess-site/src/app/layout.tsx
@@ -3,7 +3,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
-
+import { Analytics } from "@vercel/analytics/react"
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -14,7 +14,9 @@ export default function RootLayout({
       <body
         className={`h-full w-full`}
       >
+
         <SessionProvider>
+          <Analytics />
           {children}
         </SessionProvider>
       </body>
