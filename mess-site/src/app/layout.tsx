ac8f0fceaf9c01c1189d8458b6f0175690534205
ArpitKhatri1 <arpitkumarkhatri@gmail.com>
2025-04-11 23:37:36 +0530

    added speed insights

--- mess-site/src/app/layout.tsx
+++ mess-site/src/app/layout.tsx
@@ -4,6 +4,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -17,6 +19,7 @@ export default function RootLayout({
 
         <SessionProvider>
           <Analytics />
+          <SpeedInsights />
           {children}
         </SessionProvider>
       </body>
